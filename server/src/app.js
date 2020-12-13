f6db7199955dcbfa41386d588218f69022d4f3ca
李岳芸 <957275485@qq.com>
2020-12-13 12:31:45 +0800

    跨域

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors({
-    "origin": "*",
+    "origin": "http://meeting.stable-graphene.com",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
